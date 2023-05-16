ea8bf1d2564501e2c8f96215bf82dd0718cf9291
Eduardo Celino <ehcelino@gmail.com>
2023-05-16 14:12:53 -0300

    inclusão de badge

--- spec/system/auction_lots/user_views_winning_auction_lots_spec.rb
+++ spec/system/auction_lots/user_views_winning_auction_lots_spec.rb
@@ -44,6 +44,47 @@ describe 'Usuário vê lotes vencedores' do
 
   end
 
+  it 'na sua página' do
+
+    # Arrange
+    first_admin = User.create!(name: 'John', cpf: 31887493093, email: 'john@leilaodogalpao.com.br',
+                              role: 1, password: 'password')
+    second_admin = User.create!(name: 'Daniel', cpf: 92063172021, email: 'daniel@leilaodogalpao.com.br',
+                                role: 1, password: 'password')
+    user = User.create!(name: 'Michael', cpf: 62059576040, email: 'michael@ig.com.br',
+                        role: 0, password: 'password')
+    auction_lot = AuctionLot.new(code:'XPG035410', start_date: '10/03/2023', end_date: '01/04/2023',
+                                    min_bid_amount: 300, min_bid_difference: 50, status: 7, creator: first_admin, approver: second_admin)
+    auction_lot.save!(validate: false)
+    category = Category.create!(name:'Informática')
+    item = Item.create!(name:'Mouse Logitech', description:'Mouse Gamer 1200dpi', weight: 200,
+                        width: 6, height: 3, depth: 11, category_id: category.id)
+    LotItem.create!(auction_lot_id: auction_lot.id, item_id: item.id)
+    bid = Bid.new(auction_lot_id: auction_lot.id, user_id: user.id, value: 301)
+    bid.save!(validate: false)
+    second_auction_lot = AuctionLot.new(code:'ABC035410', start_date: '20/04/2023', end_date: '01/05/2023',
+                                           min_bid_amount: 300, min_bid_difference: 50, status: 7, creator: first_admin, approver: second_admin)
+    second_auction_lot.save!(validate: false)
+    second_item = Item.create!(name:'Mouse Microsoft', description:'Mouse sem fio', weight: 200,
+                              width: 6, height: 3, depth: 11, category_id: category.id)
+    LotItem.create!(auction_lot_id: second_auction_lot.id, item_id: second_item.id)
+    bid = Bid.new(auction_lot_id: second_auction_lot.id, user_id: user.id, value: 301)
+    bid.save!(validate: false)
+
+    # Act
+    login_as user
+    visit root_path
+    click_on 'Michael - michael@ig.com.br'
+
+    # Assert
+    expect(page).to have_content 'Leilões vencedores'
+    expect(page).to have_content 'Lote: XPG035410'
+    expect(page).to have_content 'Valor: R$ 301,00'
+    expect(page).to have_content 'Lote: ABC035410'
+    expect(page).to have_content 'Valor: R$ 301,00'
+
+  end
+
   it 'e vê as propriedades de um lote' do
 
     # Arrange
@@ -73,6 +114,43 @@ describe 'Usuário vê lotes vencedores' do
     expect(page).to have_content 'Lote para leilão código XPG035410'
     expect(page).to have_content 'Status: Finalizado'
     expect(page).to have_content 'Este lote foi finalizado. Usuário vencedor: Michael - michael@ig.com.br'
+
+  end
+
+
+  it 'na badge' do
+
+    # Arrange
+    first_admin = User.create!(name: 'John', cpf: 31887493093, email: 'john@leilaodogalpao.com.br',
+                              role: 1, password: 'password')
+    second_admin = User.create!(name: 'Daniel', cpf: 92063172021, email: 'daniel@leilaodogalpao.com.br',
+                                role: 1, password: 'password')
+    user = User.create!(name: 'Michael', cpf: 62059576040, email: 'michael@ig.com.br',
+                        role: 0, password: 'password')
+    auction_lot = AuctionLot.new(code:'XPG035410', start_date: '10/03/2023', end_date: '01/04/2023',
+                                    min_bid_amount: 300, min_bid_difference: 50, status: 7, creator: first_admin, approver: second_admin)
+    auction_lot.save!(validate: false)
+    category = Category.create!(name:'Informática')
+    item = Item.create!(name:'Mouse Logitech', description:'Mouse Gamer 1200dpi', weight: 200,
+                        width: 6, height: 3, depth: 11, category_id: category.id)
+    LotItem.create!(auction_lot_id: auction_lot.id, item_id: item.id)
+    bid = Bid.new(auction_lot_id: auction_lot.id, user_id: user.id, value: 301)
+    bid.save!(validate: false)
+    second_auction_lot = AuctionLot.new(code:'ABC035410', start_date: '20/04/2023', end_date: '01/05/2023',
+                                           min_bid_amount: 300, min_bid_difference: 50, status: 7, creator: first_admin, approver: second_admin)
+    second_auction_lot.save!(validate: false)
+    second_item = Item.create!(name:'Mouse Microsoft', description:'Mouse sem fio', weight: 200,
+                              width: 6, height: 3, depth: 11, category_id: category.id)
+    LotItem.create!(auction_lot_id: second_auction_lot.id, item_id: second_item.id)
+    bid = Bid.new(auction_lot_id: second_auction_lot.id, user_id: user.id, value: 301)
+    bid.save!(validate: false)
+
+    # Act
+    login_as user
+    visit root_path
+
+    # Assert
+    expect(page).to have_css '#badge', text: '2'
 
   end
 

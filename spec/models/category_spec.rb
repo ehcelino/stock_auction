bbfccae5d7daeb1e4a6bbde7be54cb87408a0913
Eduardo Celino <ehcelino@gmail.com>
2023-05-26 16:39:19 -0300

    testes de categoria

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  describe '#valid?' do
+    it 'duplicado' do
+      # Arrange
+      Category.create!(name:'Informática')
 
+      # Act
+      category = Category.new(name:'Informática')
+      result = category.valid?
+
+      # Assert
+      expect(result).to eq false
+      expect(category.errors[:name]).to include 'já está em uso'
+    end
+
+    it 'campos vazios' do
+      # Arrange
+
+      # Act
+      category = Category.new(name:'')
+      result = category.valid?
+
+      # Assert
+      expect(result).to eq false
+      expect(category.errors[:name]).to include 'não pode ficar em branco'
+    end
+  end
 end
